c062f9f55da80e27e50faf23dafee714b45cacfd
tarunsharma1999 <ts5153033@gmail.com>
2019-07-12 04:12:20 +0530

    updated css,image,script

---
+++ php/storeImage.php
@@ -0,0 +1,19 @@
+<?php
+
+    $User_name= $_POST['User_Id'];
+	$img = $_POST['image'];
+	$image_parts = explode(";base64,", $img);
+	$base64_image= $image_parts[1];
+    $con = mysqli_connect('localhost','root','','xpotify_db');
+   
+   if(!$con)
+   {
+       die("database not connected");
+   }
+   
+   $query= "INSERT INTO USERS (User_Name,User_pic) VALUES  ('$User_name','$base64_image')";
+
+   $add_res = mysqli_query($con, $query) or die(mysqli_error($con));
+	
+
+?>
